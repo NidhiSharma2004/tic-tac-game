35b71430628d3f15e5a449151179ada0920abc99
NidhiSharma2004 <vidhi6643@gmail.com>
2022-02-13 18:23:47 +0530

    double print cross icn fixed

--- assets/main.js
+++ assets/main.js
@@ -1,22 +1,30 @@
 var container = document.querySelector(".container");
-var items = document.querySelectorAll(".item")
-console.log(items);
-// items.forEach(function(item){
-//   if(item){
-//     item.addEventListener("click",(e)=>{
-//         console.log(e.target)
-//        var zeroicn = e.target.children[0]
-//        zeroicn.classList.add("show")
-//     })
-    
-//   }
-// });
-items.forEach(function(item){
-    if(item){
-      item.addEventListener("click",()=>{
-        var zeroicn=item.childNodes[1];
-        zeroicn.classList.add("show")
-      })
-      
+var items = document.querySelectorAll(".item");
+items.forEach(function (item) {
+  if (item) {
+    item.addEventListener("click", function () {
+      var zeroicn = item.childNodes[1];
+      zeroicn.classList.add("show");
+    var randomId = Math.floor((Math.random()) * 10);
+    var closeIcn = container.children[randomId].lastElementChild;
+    var firstChild = container.children[randomId].firstElementChild;
+
+    console.log(closeIcn.classList);
+    console.log(firstChild.classList);
+    // closeIcn.classList.add("show");
+    if(firstChild.classList.contains("show")){
+        // closeIcn.classList.add("show");
+    }else{
+        closeIcn.classList.add("show");
     }
-  });
+    // console.log(randomId)
+    });
+  }
+});
+
+
+// function randomNumber() {
+//   var randomId = Math.floor((Math.random()) * 10);
+// //   console.log(container.children[1]);
+// console.log(randomId)
+// }

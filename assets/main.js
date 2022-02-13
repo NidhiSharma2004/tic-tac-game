121c205cddf4012787548a5a1e0eeb358c935726
NidhiSharma2004 <vidhi6643@gmail.com>
2022-02-13 17:27:06 +0530

    zeroIcon bug fixed

--- assets/main.js
+++ assets/main.js
@@ -1,15 +1,22 @@
 var container = document.querySelector(".container");
-// var zeroIcn = document
-// console.log()
 var items = document.querySelectorAll(".item")
 console.log(items);
+// items.forEach(function(item){
+//   if(item){
+//     item.addEventListener("click",(e)=>{
+//         console.log(e.target)
+//        var zeroicn = e.target.children[0]
+//        zeroicn.classList.add("show")
+//     })
+    
+//   }
+// });
 items.forEach(function(item){
-  if(item){
-    item.addEventListener("click",(e)=>{
-       var zeroicn = e.target.children[0]
-       zeroicn.classList.add("show")
-    })
-  }
-  
-  
-});
+    if(item){
+      item.addEventListener("click",()=>{
+        var zeroicn=item.childNodes[1];
+        zeroicn.classList.add("show")
+      })
+      
+    }
+  });

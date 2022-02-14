1ab3ee55c4d36cf5b19d67b4de48d683640691d4
NidhiSharma2004 <vidhi6643@gmail.com>
2022-02-14 09:14:03 +0530

    icons bugs fixed

--- assets/main.js
+++ assets/main.js
@@ -5,18 +5,29 @@ items.forEach(function (item) {
     item.addEventListener("click", function () {
       var firstChildNode = item.childNodes[1];
       var secondChildNode = item.childNodes[3];
-      if(!secondChildNode.classList.contains("show")){
-        firstChildNode.classList.add("show")
-      }else{
-
+      if (!secondChildNode.classList.contains("show")) {
+        firstChildNode.classList.add("show");
+      } else {
       }
-    var randomId = Math.floor((Math.random()) * 10);
-    var closeIcn = container.children[randomId].lastElementChild;
-    var firstChild = container.children[randomId].firstElementChild;
-    if(firstChild.classList.contains("show")){
-    }else{
-        closeIcn.classList.add("show");
-    }
+      items.forEach(function (lastItem) {
+        if (lastItem == item) {
+        }
+      });
+      var randomId = Math.floor(Math.random() * 10);
+      console.log(randomId)
+      var lastChild = container.children[randomId].lastElementChild;
+      var firstChild = container.children[randomId].firstElementChild;
+      setTimeout(() => {
+        if (
+          firstChild.classList.contains("show") ||
+          lastChild.classList.contains("show")
+        ) {
+        } else {
+          lastChild.classList.add("show");
+        }
+      }, 200);
     });
   }
 });
+
+function winnner() {}
